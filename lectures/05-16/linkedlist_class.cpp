08806cf23af652b3d214b7079a4e5c32f5747e23
Gavin T <gavintranquilino@gmail.com>
2024-05-16 15:05:09 -0400

    linkedlist stuff

--- lectures/05-16/linkedlist_class.cpp
+++ lectures/05-16/linkedlist_class.cpp
@@ -6,11 +6,43 @@ struct Node
     Node* link; 
 };
 
-Typedef Node* NodePtr;
+typedef Node* NodePtr;
+
+class LinkedList
+{
+private:
+public:
+};
+
+void headInsert(NodePtr& head, int theData);
+void printAllNodes(NodePtr& head);
 
 int main()
 {
     NodePtr head = new Node;
-    
+    headInsert(head, 1);
+    headInsert(head, 2);
+    printAllNodes(head);
+
+    // TODO: reads 1 more than needed
+
     return 0;
+}
+
+void headInsert(NodePtr& head, int theData)
+{
+    NodePtr tempPtr;
+    tempPtr = new Node;
+    tempPtr->data = theData;
+    tempPtr->link = head;
+    head = tempPtr;
+}
+
+void printAllNodes(NodePtr& head)
+{
+    while (head != NULL)
+    {
+        std::cout << head->data << '\n';
+        head = head->link;
+    }
 }
