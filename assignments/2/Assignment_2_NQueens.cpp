982bee026c2379bd90e4e9721521ca072d8e7790
Gavin T <gavintranquilino@gmail.com>
2024-07-04 16:11:30 -0400

    Working program

--- assignments/2/Assignment_2_NQueens.cpp
+++ assignments/2/Assignment_2_NQueens.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <stack>
+#include <cmath>
 
-const unsigned N = 5;
+const unsigned N = 8;
 
 struct Square
 {
@@ -10,23 +12,31 @@ struct Square
     Square(unsigned x_i, unsigned y_i): x(x_i), y(y_i) {}
 };
 
-void drawBoard(std::stack<Square> stack);
+void drawBoard(std::stack<Square>& stack, std::ofstream& fileOut);
+bool isSafe(unsigned row, unsigned col, std::stack<Square>& stack);
+bool solve(std::stack<Square>& stack, unsigned row);
 
 int main()
 {
     std::stack<Square> stack;
-    stack.push(Square(0, 0));
-    stack.push(Square(2, 1));
-    stack.push(Square(4, 2));
-    stack.push(Square(1, 3));
-    stack.push(Square(3, 4));
+    std::ofstream outputFile;
 
-    drawBoard(stack);
+    outputFile.open("board.txt");
 
+    if (!outputFile)
+    {
+        std::cout << "Unable to open file\n";
+        return 1;
+    }
+
+    solve(stack, 0);
+    drawBoard(stack, outputFile);
+
+    outputFile.close();
     return 0;
 }
 
-void drawBoard(std::stack<Square> stack)
+void drawBoard(std::stack<Square>& stack, std::ofstream& fileOut)
 {
     bool board[N][N] = {0};
 
@@ -39,13 +49,66 @@ void drawBoard(std::stack<Square> stack)
     }
 
     // print board
-    for (int i=0; i<N; i++)
+    for (int row=0; row<N; row++)
     {
-        std::cout << i << " | ";
-        for (int j=0; j<N; j++)
+        std::cout << row << " | ";
+        for (int col=0; col<N; col++)
         {
-            std::cout << board[i][j] << ' ';
+            std::cout << board[row][col] << ' ';
+            fileOut << board[row][col] << ' ';
         }
         std::cout << '\n';
+        fileOut << '\n';
+    }
+}
+
+bool isSafe(unsigned row, unsigned col, std::stack<Square>& stack) 
+{
+    std::stack<Square> temp = stack;
+
+    while (!temp.empty())
+    {
+        Square queen = temp.top();
+        temp.pop();
+
+        // diagonal check || column check
+        if ((abs(queen.x - row) == abs(queen.y - col)) || (queen.y == col))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool solve(std::stack<Square>& stack, unsigned row)
+{
+    // base case (if looking at last row) or (if looking at last row on 3x3 grid) or (looking at last row on 2x2 grid):
+    if (row == N || (row == 2 && N == 3) || (row == 1 && N == 2))
+    {
+        return true;
+    }
+
+    // recursive case:
+    else
+    {
+        for (unsigned col=0; col<N; col++)
+        {
+            if (isSafe(row, col, stack))
+            {
+                std::cout << '(' << row << ", " << col << ')' << '\n';
+                stack.push(Square(row, col));
+
+                // check next row below, if not safe on any, then pop the last queen and find a new queen location
+                if (solve(stack, row+1)) 
+                {
+                    return true;
+                }
+
+                std::cout << "GO BACK" << '\n';
+                stack.pop(); // backtrack if solve from above failed (row != N)
+            }            
+        }
+        return false;
     }
 }
